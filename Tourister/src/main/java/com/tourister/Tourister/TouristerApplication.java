a86c8bc2adeee51c42cb2ce3a7acfb7469a9a146
unknown <rohithkumar.m@codeboardtech.com>
2024-12-02 07:23:04 +0530

    Learning Actuator,Access end point and secure the end points, custom properties

---
+++ Tourister/src/main/java/com/tourister/Tourister/TouristerApplication.java
@@ -0,0 +1,13 @@
+package com.tourister.Tourister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class TouristerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristerApplication.class, args);
+	}
+}
